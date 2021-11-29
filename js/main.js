3e8cc44a576b718c8ca18db8c8c9961337ce32cd
am1rcek <xusandadajonov4385@gmail.com>
2021-11-29 09:00:59 +0500

    awdawdaw

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let elInp = document.getElementById("search");
+let elSearchHistory = document.querySelector(".search-history");
+let elForm = document.querySelector(".header__form");
+let elBurgerBtn = document.querySelector(".header__burger--btn");
+let elSmallMenu = document.querySelector(".menu__small-menu");
+let elBigMenu = document.querySelector(".big-menu");
+let elContents = document.querySelector(".contents");
+let elVidios = document.querySelector(".videos");
+let sum = 1;
+
+//events
+
+elInp.addEventListener("keyup", showSearchHistory);
+elForm.addEventListener("submit", formFunc);
+elBurgerBtn.addEventListener("click", showBigMenu)
+
+//functions
+function showSearchHistory(e) {
+    elSearchHistory.style.display = "flex";
+
+    if(elInp.value == ""){
+        elSearchHistory.style.display = "none";
+    }
+}
+
+function formFunc(e){
+    e.preventDefault();
+
+    let elItem = document.createElement("li");
+    elItem.className = "search-history__item";
+    elItem.innerHTML = elInp.value;
+    elSearchHistory.append(elItem)
+    elInp.value = "";
+}
+
+function showBigMenu(e){
+    if(sum){
+        elSmallMenu.style.display = "none";
+        elBigMenu.style.display = "flex";
+        elContents.style.paddingLeft = "225px";
+        elVidios.style.paddingRight = "40px";
+        sum = 0;
+    }
+    else{
+        elSmallMenu.style.display = "flex";
+        elBigMenu.style.display = "none";
+        elContents.style.paddingLeft = "0";
+        elVidios.style.paddingRight = "80px";
+        console.log("awd");
+        sum = 1;
+    }
+    console.log(sum);
+}
